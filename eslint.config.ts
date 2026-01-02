c934294f1cf1dc3827c555518d24ae23ea348519
Anin Pure <pureanin@gmail.com>
2026-01-03 06:53:11 +0800

    fix(eslint): 添加typescript配置中的tsconfig路径

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,11 @@
 import { useConfig } from './src';
 
 export default useConfig(
-  {},
+  {
+    typescript: {
+      tsconfigPath: './tsconfig.json',
+    },
+  },
   {
     files: ['tsdown.config.ts'],
     rules: { 'import/no-extraneous-dependencies': 'off' },
